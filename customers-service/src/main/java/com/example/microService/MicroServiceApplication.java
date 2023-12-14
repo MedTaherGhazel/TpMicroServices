6ed3df26600c953014a3042fb3dc23f0dac24558
Med Taher Ghazel <119009624+MedTaherGhazel@users.noreply.github.com>
2023-12-14 22:41:15 +0100

    keycloack-config

--- customers-service/src/main/java/com/example/microService/MicroServiceApplication.java
+++ customers-service/src/main/java/com/example/microService/MicroServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+//@EnableWebSecurity
 @SpringBootApplication
 public class MicroServiceApplication {
 
